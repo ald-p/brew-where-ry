ba0aa21db21c4015414acfc816610ae4074b6ebf
Aldwin <aldwin.pag@gmail.com>
2024-05-17 10:41:51 -0400

    create logo component

---
+++ src/components/core/Logo.jsx
@@ -0,0 +1,12 @@
+import { RiBeerFill } from 'react-icons/ri';
+
+const Logo = () => {
+  return (
+    <a className="flex items-center justify-center text-yellow-50 text-4xl p-4 cursor-pointer">
+      <RiBeerFill className="mr-2" />
+      <h1>brew-where?-ry</h1>
+    </a>
+  );
+};
+
+export default Logo;
